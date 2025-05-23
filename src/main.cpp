5d68d228029af9e35a53c2f45960905fe00e5286
淼涛 温 <w1084349470@outlook.com>
2025-05-23 21:42:14 +0800

    servo well done!!!

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,30 @@
 #include "config"
 #include "conponent"
 
+void self_test()
+{
+    servo_tong.TurnMax();
+    delay(1000);
+    servo_tong.TurnMin();
+    delay(1000);
+
+    servo_cleave.TurnMin();
+    delay(1000);
+    servo_cleave.TurnMax();
+    delay(1000);
+
+    servo_neck.TurnMax();
+    delay(1000);
+    servo_neck.TurnMin();
+    delay(1000);
+}
+
 void setup()
 {
-    
+    delay(200);
 }
 
 void loop()
 {
-    // tick();
-    servo_cleave.SetPulse(1500);
+    self_test();
 }
