981f8eca451074b3c2a04f7d4a80e3d8e494ece9
淼涛 温 <w1084349470@outlook.com>
2025-05-24 21:13:07 +0800

    partly remoted

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,56 @@ void self_test()
 
 void setup()
 {
-    delay(200);
+    delay(100);
+    self_test();
     Serial.begin(9600);
     // motor_l.Forward();
     // motor_r.Forward();
+    delay(100);
 }
-
+int cmd;
 void loop()
 {
-    // self_test();
-    
+    cmd=Serial.parseInt();
+    if(cmd)
+    {
+        switch (cmd)
+        {
+        case CMD_NECK_OFF:
+            servo_neck.TurnMin();
+            break;
+        case CMD_NECK_MID:
+            servo_neck.SetAngle(SERVO_NECK_MID);
+            break;
+        case CMD_NECK_ON:
+            servo_neck.TurnMax();
+            break;
+        case CMD_CLEAVE_OFF:
+            servo_cleave.TurnMin();
+            break;
+        case CMD_CLEAVE_ON:
+            servo_cleave.TurnMax();
+        case CMD_TONG_OFF:
+            servo_tong.TurnMin();
+            break;
+        case CMD_TONG_ON:
+            servo_tong.TurnMax();
+            break;
+
+        case CMD_MOTOR_L_F:
+            motor_l.Forward();
+            break;
+        case CMD_MOTOR_L_B:
+            motor_l.Backward();
+        case CMD_MOTOR_R_F:
+            motor_r.Forward();
+            break;
+        case CMD_MOTOR_R_B:
+            motor_r.Backward();
+            break;
+        default:
+            break;
+        }
+    }
+    cmd=0;
 }
