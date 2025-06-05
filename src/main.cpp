547b791c41e9791a177662df3dcb977b4d9f0b90
淼涛 温 <w1084349470@outlook.com>
2025-06-05 17:29:07 +0800

    remove self_test

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,10 @@
 #include "conponent"
 #include "commands"
 
-void self_test()
-{
-    servo_tong.TurnMax();
-    delay(1000);
-    servo_tong.TurnMin();
-    delay(1000);
-
-    servo_cleave.TurnMin();
-    delay(500);
-    servo_cleave.TurnMax();
-    delay(500);
-
-    servo_neck.TurnMax();
-    delay(500);
-    servo_neck.TurnMin();
-    delay(500);
-}
-
 void setup()
 {
     delay(100);
-    // self_test();
     Serial.begin(9600);
-    // motor_l.Forward();
-    // motor_r.Forward();
     delay(100);
 }
 int cmd;
